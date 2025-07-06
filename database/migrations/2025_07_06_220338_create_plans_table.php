c7f5faf643a220506c4a965c49319270032a4613
easylearningbd <elebdsoft@gmail.com>
2025-07-07 04:10:27 +0600

    20 Subscription Plans CRUD Setup Part 1

---
+++ database/migrations/2025_07_06_220338_create_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('token_limit')->default(100); // Total Free Tokens
+            $table->integer('template_limit')->default(2);
+            $table->decimal('price',8 ,2)->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};

259246c1418cb31e35ae26fa376949ba28cfe722
easylearningbd <elebdsoft@gmail.com>
2025-07-10 03:44:37 +0600

    38 Setup Claude Service in Laravel Part 2

--- app/services/ClaudeService.php
+++ app/services/ClaudeService.php
@@ -14,6 +14,45 @@ class ClaudeService {
         Log::info('Cladue API KEY: ' . $this->apiKey);
     }
 
+public function generateWebsite(string $prompt, array $context = []): array {
+    try {
+       $systemPrompt = $this->buildSystemPrompt();
+       $userPrompt = $this->buildUserPrompt($prompt,$context);
+
+       $response = Http::withHeaders([
+            'x-api-key' => $this->apiKey,
+            'content-type' => 'application/json',
+            'anthropic-version' => '2023-06-01'
+       ])->timeout(120)->post($this->$baseUrl . '/v1/messages',[
+            'model' => 'claude-3-5-sonnet-20241022',
+            'max_tokens' => 4000,
+            'system' => $systemPrompt,
+            'messages' => [
+                [
+                    'role' => 'user',
+                    'content' => $userPrompt
+                ] 
+            ]
+                ]);
+        if ($response->successful()) {
+           $data = $response->json();
+           return $this->parseClaudeResponse($data['content'][0]['text'], $context);
+        }
+        throw new \Exception('Claude API Request failed' . $response->body());
+
+    } catch (\Exception $e) {
+        Log::error('Claude API Error:' . $e->getMessage());
+        throw $e;
+    }  
+}
+
+private function buildSystemPrompt(): string {
+    
+}
+
+
+
+
 
 
 

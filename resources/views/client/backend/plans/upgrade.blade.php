2eaf0fddfdfb76ac4010bfed57372334cb34b770
easylearningbd <elebdsoft@gmail.com>
2025-07-13 03:20:23 +0600

    65 User Profile Payment and Package Setup Part 2

--- resources/views/client/backend/plans/upgrade.blade.php
+++ resources/views/client/backend/plans/upgrade.blade.php
@@ -7,43 +7,54 @@
                         
 <div class="row mt-sm-4 align-items-end justify-content-center my-3">
    
+    @foreach ($plans as $plan)  
     <div class="col-lg-3">
         <div class="card h-100 overflow-hidden">
             <div class="card-header border-bottom border-dashed p-3 text-center">
-                <h4 class="fw-bold fs-18">Free Plan</h4>
-                <p class="mt-2 mb-0 text-muted">Tailored for
-                    individual professionals and
-                    hobbyists.</p>
+                <h4 class="fw-bold fs-18">{{ $plan->name }} Plan</h4>
+<p class="mt-2 mb-0 text-muted">
+    @if ($plan->name === 'Free')
+        Try it out and start earning before you pay.
+    @elseif ($plan->name === 'Starter')
+The foundational tools and support you need to build your business.
+    @else
+    Advanced tools and more support to help you scale.
+    @endif
+    </p>
             </div>
             <div class="card-body p-3">
                 <div class="d-flex align-items-center gap-1">
                     <span class="text-muted fs-3 fw-semibold">$</span>
-                    <h1 class="display-5 fw-semibold mb-0">0</h1>
+                    <h1 class="display-5 fw-semibold mb-0">{{ $plan->price }}</h1>
                     <div class="d-block">
-                        <p class=" fw-semibold mb-0">One-time payment</p>
-                        <p class="text-muted mb-0">+plus local taxes</p>
+                        <p class=" fw-semibold mb-0">/Month</p>
+                        
                     </div>
                 </div>
                 <ul class="d-flex flex-column gap-2 mt-3 list-unstyled">
                     <li>
                         <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                        Single user license
+                        Token Limit {{ $plan->token_limit }} 
                     </li>
                     <li>
                         <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                        Access to some components
+                        Template Limit {{ $plan->template_limit }} 
                     </li>
                     <li>
                         <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
                         Lifetime access
                     </li>
-                    <li>
-                        <i class="ti ti-point text-primary fs-4 align-middle me-1"></i>
-                        Single project
-                    </li>
+                     
                     <li>
                         <i class="ti ti-point text-muted fs-4 align-middle me-1"></i>
-                        No support
+                @if ($plan->name === 'Free')
+                      No support
+                @elseif ($plan->name === 'Starter')
+                      Email support
+                @else
+                      Full support
+                @endif
+                      
                     </li>
                     <li>
                         <i class="ti ti-point text-muted fs-4 align-middle me-1"></i>
@@ -53,11 +64,21 @@
                 </ul>
             </div>
             <div class="card-footer">
-                <a href="#!" class="btn btn-primary fw-semibold w-100">Buy
-                    Now</a>
+
+    @if ($plan->name === Auth::user()->plan->name)
+        <button class="btn btn-secondary fw-semibold w-100" disabled>Current Plan</button>
+
+    @else 
+      <a href="" class="btn {{ $plan->name === 'Starter' ? 'btn-danger' : 'btn-primary' }} fw-semibold w-100 ">Buy Now</a> 
+    @endif 
+
+                
             </div>
         </div>
     </div>
+     @endforeach
+
+
      
 </div>
 </div> <!-- end col-->

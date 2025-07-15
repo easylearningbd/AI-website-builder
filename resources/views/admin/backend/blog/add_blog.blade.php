1bbb0cf120e76093930ac3ddef6730a6472c6d0a
easylearningbd <elebdsoft@gmail.com>
2025-07-16 02:04:14 +0600

    79 Admin Generate Content for Blog Part 3

--- resources/views/admin/backend/blog/add_blog.blade.php
+++ resources/views/admin/backend/blog/add_blog.blade.php
@@ -1,6 +1,12 @@
 @extends('admin.admin_master')
 @section('admin')
 
+<!-- Include Quill CSS -->
+<link href="https://cdn.quilljs.com/1.3.7/quill.snow.css" rel="stylesheet">
+
+<!-- Include Quill JS -->
+<script src="https://cdn.quilljs.com/1.3.7/quill.js"></script>
+
 <div class="row">
     <div class="col-12">
         <div class="card">
@@ -19,7 +25,30 @@
            <label for="input2" class="form-label"> </label>
            <button class="btn btn-primary">Generate</button> 
         </div>
+
+        <div class="mb-3 col-md-12">
+            <div id="editor" style="height :400px;" > </div> 
+        </div>
+
+        <div class="mb-3 col-md-6">
+             <label for="category" class="form-label">Category </label>
+            <select name="category" id="category" class="form-select">
+                <option value="" selected="">Select Category</option>
+                <option value="AI">AI</option>
+                <option value="Software">Software</option>
+                <option value="ChatGpt">ChatGpt</option>
+                <option value="Gemini">Gemini</option>
+                <option value="Claude">Claude</option> 
+            </select>
+        </div>
+
+       <div class="mb-3 col-md-6">
+            <label for="input2" class="form-label">Image</label>
+            <input class="form-control" name="image" type="file" id="image" > 
+        </div> 
       </div>
+
+    <button class="btn btn-info">Save to Database</button>
 
     </div>
 
@@ -29,7 +58,20 @@
 </div> 
 
 
+<script>
+     var quill = new Quill('#editor', {
+        theme: 'snow',
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'underline', 'link'],
+                [{'list': 'ordered'}, {'list': 'bullet'}],
+                ['image']
+            ]
+        }
+    });
 
+
+</script>
 
 
 

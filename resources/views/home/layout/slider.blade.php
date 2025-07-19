ee38c3ce327423aa63c02698ed737739662cb24e
easylearningbd <elebdsoft@gmail.com>
2025-07-20 03:50:02 +0600

    94 Setup Frontend Slider Part 3

--- resources/views/home/layout/slider.blade.php
+++ resources/views/home/layout/slider.blade.php
@@ -1,10 +1,15 @@
- <section class="cs_hero cs_style_1 cs_type_3 position-relative" id="home">
+
+@php
+  $slider = App\Models\Slider::find(1);
+@endphp
+
+<section class="cs_hero cs_style_1 cs_type_3 position-relative" id="home">
       <div class="container">
         <div class="cs_hero_content">
           <div class="row align-items-center cs_gap_y_40">
             <div class="col-lg-5 order-lg-2">
               <div class="cs_hero_thumbnail cs_radius_15 position-relative wow fadeInRight">
-                <img src="{{ asset('frontend/assets/img/hero_img_4.jpg') }}" alt="Hero Image">
+                <img src="{{ asset($slider->image) }}" alt="Hero Image">
                 <div class="cs_hero_shape_vector position-absolute">
                   <img src="{{ asset('frontend/assets/img/quarter-circle.svg') }}" alt="Vector Shape">
                 </div>
@@ -18,15 +23,15 @@
             </div>
             <div class="col-lg-7 order-lg-1">
               <div class="cs_hero_text position-relative z-2">
-                <h1 class="cs_hero_title cs_fs_68 wow fadeInDown">Elevate Ur Customer Relationships with Our <span class="position-relative">
-                  CRM
+                <h1 class="cs_hero_title cs_fs_68 wow fadeInDown"> {{ $slider->title }} <span class="position-relative">
+                  
                   <img src="{{ asset('frontend/assets/img/vector.svg') }}" alt="Vector line" class="cs_line_1 position-absolute">
                   <img src="{{ asset('frontend/assets/img/vector-2.svg') }}" alt="Vector line" class="cs_line_2 position-absolute">
                   </span>
                 </h1>
-                <p class="cs_hero_subtitle">Enhance your customer interactions and streamline your sales processes with our powerful and intuitive CRM solution.</p>
+                <p class="cs_hero_subtitle">{{ $slider->description }}</p>
                 <div class="cs_btn_group position-relative">
-                  <a href="login.html" aria-label="Sign in button" class="cs_btn cs_style_1 cs_purple_bg cs_white_color wow fadeInLeft"><span>Get a Demo</span></a>
+                  <a href="{{ $slider->link }}" aria-label="Sign in button" class="cs_btn cs_style_1 cs_purple_bg cs_white_color wow fadeInLeft"><span>Get a Demo</span></a>
                   <a href="register.html" aria-label="Register free button" class="cs_btn cs_style_1 cs_outline cs_semibold cs_heading_color wow fadeInRight">
                   <span class="cs_player_text">Get Started Free</span>
                   </a>

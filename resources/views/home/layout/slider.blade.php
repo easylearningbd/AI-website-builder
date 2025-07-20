4c3666989a57b8137d75fbe339800ca26e15f356
easylearningbd <elebdsoft@gmail.com>
2025-07-20 22:36:14 +0600

    95 Setup Frontend Slider With JavaScript Part 1

--- resources/views/home/layout/slider.blade.php
+++ resources/views/home/layout/slider.blade.php
@@ -9,7 +9,12 @@
           <div class="row align-items-center cs_gap_y_40">
             <div class="col-lg-5 order-lg-2">
               <div class="cs_hero_thumbnail cs_radius_15 position-relative wow fadeInRight">
-                <img src="{{ asset($slider->image) }}" alt="Hero Image">
+
+                <img id="appImage" src="{{ asset($slider->image) }}" alt="Hero Image" style="cursor: pointer">
+                @if (auth()->check())
+ <input type="file" id="uploadImage" style="display:none">                  
+                @endif
+
                 <div class="cs_hero_shape_vector position-absolute">
                   <img src="{{ asset('frontend/assets/img/quarter-circle.svg') }}" alt="Vector Shape">
                 </div>
@@ -23,13 +28,16 @@
             </div>
             <div class="col-lg-7 order-lg-1">
               <div class="cs_hero_text position-relative z-2">
-                <h1 class="cs_hero_title cs_fs_68 wow fadeInDown"> {{ $slider->title }} <span class="position-relative">
-                  
-                  <img src="{{ asset('frontend/assets/img/vector.svg') }}" alt="Vector line" class="cs_line_1 position-absolute">
-                  <img src="{{ asset('frontend/assets/img/vector-2.svg') }}" alt="Vector line" class="cs_line_2 position-absolute">
-                  </span>
-                </h1>
-                <p class="cs_hero_subtitle">{{ $slider->description }}</p>
+                
+  <h1 class="cs_hero_title cs_fs_68 wow fadeInDown editable-title" contenteditable={{ auth()->check() && auth()->user()->role === 'admin' ? 'true' : 'false' }} data-id="{{ $slider->id }}"> {{ $slider->title }} <span class="position-relative">
+    
+    <img src="{{ asset('frontend/assets/img/vector.svg') }}" alt="Vector line" class="cs_line_1 position-absolute">
+    <img src="{{ asset('frontend/assets/img/vector-2.svg') }}" alt="Vector line" class="cs_line_2 position-absolute">
+    </span>
+  </h1>
+
+  <p class="cs_hero_subtitle editable-description" contenteditable={{ auth()->check() && auth()->user()->role === 'admin' ? 'true' : 'false' }} data-id="{{ $slider->id }}">{{ $slider->description }}</p>
+
                 <div class="cs_btn_group position-relative">
                   <a href="{{ $slider->link }}" aria-label="Sign in button" class="cs_btn cs_style_1 cs_purple_bg cs_white_color wow fadeInLeft"><span>Get a Demo</span></a>
                   <a href="register.html" aria-label="Register free button" class="cs_btn cs_style_1 cs_outline cs_semibold cs_heading_color wow fadeInRight">

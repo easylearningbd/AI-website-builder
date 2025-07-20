714883640d10d44e718784db426f78da09e56fd2
easylearningbd <elebdsoft@gmail.com>
2025-07-21 03:49:43 +0600

    102 Show Project Reviews Frontend Part 4

--- resources/views/home/layout/testimonial.blade.php
+++ resources/views/home/layout/testimonial.blade.php
@@ -1,3 +1,6 @@
+ @php
+   $review = App\Models\Review::latest()->limit(5)->get();
+ @endphp
  <section>
       <div class="cs_height_120 cs_height_lg_70"></div>
       <div class="container">
@@ -8,18 +11,20 @@
       <div class="cs_slider cs_style_1 cs_slider_gap_24">
         <div class="cs_slider_container" data-autoplay="0" data-loop="1" data-speed="800" data-center="1" data-variable-width="1" data-slides-per-view="responsive" data-xs-slides="1" data-sm-slides="2" data-md-slides="2" data-lg-slides="4" data-add-slides="4">
           <div class="cs_slider_wrapper">
+            
+          @foreach ($review as $item) 
             <div class="cs_slide">
               <div class="cs_testimonial cs_style_1 cs_radius_15 position-relative">
                 <div class="cs_avatar cs_style_1 cs_mb_41">
                   <span class="cs_avatar_icon cs_center cs_radius_100">
-                  <img src="assets/img/avatar-1.jpg" alt="Avatar">
+                  <img src="{{ asset($item->image) }}" alt="Avatar">
                   </span>
                   <div class="cs_avatar_info">
-                    <h3 class="cs_fs_21 fw-normal">Sara Thomas</h3>
-                    <p class="mb-0">Project Manager</p>
+                    <h3 class="cs_fs_21 fw-normal">{{ $item->name }}</h3>
+                    <p class="mb-0">{{ $item->post }}</p>
                   </div>
                 </div>
-                <blockquote>Gosaas has completely transformed how we manage projects at our company. The intuitive interface makes it easy for everyone to get on board, and the powerful features like subtasks and dependencies.</blockquote>
+                <blockquote>{{ $item->message }}</blockquote>
                 <div class="cs_rating" data-rating="5">
                   <div class="cs_rating_percentage"></div>
                 </div>
@@ -27,82 +32,9 @@
                 </span>
               </div>
             </div>
-            <div class="cs_slide">
-              <div class="cs_testimonial cs_style_1 cs_radius_15 position-relative">
-                <div class="cs_avatar cs_style_1 cs_mb_41">
-                  <span class="cs_avatar_icon cs_center cs_radius_100">
-                  <img src="assets/img/avatar-2.jpg" alt="Avatar">
-                  </span>
-                  <div class="cs_avatar_info">
-                    <h3 class="cs_fs_21 fw-normal">Wizard Bona</h3>
-                    <p class="mb-0">Creative Director</p>
-                  </div>
-                </div>
-                <blockquote>Managing a creative team comes with its own set of challenges, but Gosaas has made it so much easier. The customizable views allow us to organize tasks in a way that makes sense for our projects.</blockquote>
-                <div class="cs_rating" data-rating="5">
-                  <div class="cs_rating_percentage"></div>
-                </div>
-                <span class="cs_quote_icon position-absolute">                
-                </span>
-              </div>
-            </div>
-            <div class="cs_slide">
-              <div class="cs_testimonial cs_style_1 cs_radius_15 position-relative">
-                <div class="cs_avatar cs_style_1 cs_mb_41">
-                  <span class="cs_avatar_icon cs_center cs_radius_100">
-                  <img src="assets/img/avatar-3.jpg" alt="Avatar">
-                  </span>
-                  <div class="cs_avatar_info">
-                    <h3 class="cs_fs_21 fw-normal">Jhon Kubera</h3>
-                    <p class="mb-0">Marketing Specialist</p>
-                  </div>
-                </div>
-                <blockquote>As a marketing specialist, staying organized and on top of deadlines is crucial. Gosaas has been a lifesaver. The platform is incredibly user-friendly, and I love how I can customize task lists to fit my workflow.</blockquote>
-                <div class="cs_rating" data-rating="4.5">
-                  <div class="cs_rating_percentage"></div>
-                </div>
-                <span class="cs_quote_icon position-absolute">                
-                </span>
-              </div>
-            </div>
-            <div class="cs_slide">
-              <div class="cs_testimonial cs_style_1 cs_radius_15 position-relative">
-                <div class="cs_avatar cs_style_1 cs_mb_41">
-                  <span class="cs_avatar_icon cs_center cs_radius_100">
-                  <img src="assets/img/avatar-4.jpg" alt="Avatar">
-                  </span>
-                  <div class="cs_avatar_info">
-                    <h3 class="cs_fs_21 fw-normal">Emily Loren</h3>
-                    <p class="mb-0">Software Developer</p>
-                  </div>
-                </div>
-                <blockquote>Our software development team has greatly benefited from using Gosaas. The ability to set task dependencies and create subtasks allows us to break down large projects into manageable pieces.</blockquote>
-                <div class="cs_rating" data-rating="5">
-                  <div class="cs_rating_percentage"></div>
-                </div>
-                <span class="cs_quote_icon position-absolute">                
-                </span>
-              </div>
-            </div>
-            <div class="cs_slide">
-              <div class="cs_testimonial cs_style_1 cs_radius_15 position-relative">
-                <div class="cs_avatar cs_style_1 cs_mb_41">
-                  <span class="cs_avatar_icon cs_center cs_radius_100">
-                  <img src="assets/img/avatar-4.jpg" alt="Avatar">
-                  </span>
-                  <div class="cs_avatar_info">
-                    <h3 class="cs_fs_21 fw-normal">AI King</h3>
-                    <p class="mb-0">Project Manager</p>
-                  </div>
-                </div>
-                <blockquote>Gosaas has completely transformed how we manage projects at our company. The intuitive interface makes it easy for everyone to get on board, and the powerful features like subtasks and dependencies.</blockquote>
-                <div class="cs_rating" data-rating="5">
-                  <div class="cs_rating_percentage"></div>
-                </div>
-                <span class="cs_quote_icon position-absolute">                
-                </span>
-              </div>
-            </div>
+            @endforeach 
+
+           
           </div>
           <div class="cs_height_70 cs_height_lg_50"></div>
           <div class="cs_pagination cs_style_1 wow fadeInUp"></div>
